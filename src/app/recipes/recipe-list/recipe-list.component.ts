4838db6e9d3b07bc13fed89f8d0b9611ee35a024
bizwa_dan <danbizwa@gmail.com>
2023-10-23 16:47:04 +0200

    first step

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit{
+  recipes:Recipe[]=[
+    new Recipe('A recipe','this is the a recipe description','https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2VyfGVufDB8fDB8fHww'),
+    new Recipe('A recipe','this is the a recipe description','https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2VyfGVufDB8fDB8fHww'),
+  ]
+  ngOnInit(): void {
+  }
+
+}
